91ec1412b13cfff5e587a84e0138c0323cedb063
Ana Karen TovarGomez <anakgomez23@gmail.com>
2026-09-23 22:20:25 -0600

    Agregar al Carrito

--- js/carrito.js
+++ js/carrito.js
@@ -6,17 +6,7 @@
             return;
         }
 
-        let carrito = [];
-
-        try {
-            carrito = JSON.parse(sessionStorage.getItem('carrito') || '[]');
-        } catch (error) {
-            sessionStorage.removeItem('carrito');
-        }
-
-        const totalProductos = carrito.reduce((total, producto) => {
-            return total + (Number(producto.cantidad) || 0);
-        }, 0);
+        const totalProductos = Math.max(0, Number(sessionStorage.getItem('carritoContador')) || 0);
 
         contador.textContent = totalProductos;
     };
